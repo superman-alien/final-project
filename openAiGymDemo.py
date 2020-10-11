24f6588b85c7c48d655a391fc459eb96d5d0a380
mkovach2 <mkovach@bu.edu>
2020-10-11 12:08:41 -0400

    Random cartpole
    implemented a randomly behaving cartpole as per https://www.youtube.com/watch?v=8MC3y7ASoPs

---
+++ openAiGymDemo.py
@@ -0,0 +1,36 @@
+#-------10--------20--------30--------40--------50--------60--------70-------79
+
+'''
+openAiGymDemo.py
+Author: Miles Kovach
+Created: 2020-10-09
+Modified: 2020-10-11
+EC601 Assignment big, script 0.1
+
+A program to test openAIgym
+based on "Getting Started With OpenAI Gym"
+https://www.youtube.com/watch?v=8MC3y7ASoPs
+'''
+
+debug_mode = [] # initializing an empty array for debugging purposes
+
+# debug block 0 ~~~~~~~~~~~~~~~~~~~~~~~~
+
+import gym
+
+# debug block 1 ~~~~~~~~~~~~~~~~~~~~~~~~
+
+env_name = 'CartPole-v1'
+env = gym.make(env_name)
+
+env.reset()
+
+for q in range(200):
+  action = env.action_space.sample()
+  env.step(action)
+  env.render()
+  
+  if 1 in debug_mode:
+    print(q)
+    
+env.close()
