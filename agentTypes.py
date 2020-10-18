d2acd46c774525cf32ffcedd68f1479196663121
mkovach2 <mkovach@bu.edu>
2020-10-18 14:17:33 -0400

    More functionality
    supports some continuous agents now

--- agentTypes.py
+++ agentTypes.py
@@ -4,33 +4,67 @@
 agentTypes.py
 Author: Miles Kovach
 Created: 2020-10-16
-Modified: 2020-10-17
+Modified: 2020-10-18
 CLASS Assignment big, script 0.1
 
 Definition for class agent, and implementation of a few types of agents.
 To be used in a situation where openAi Gym is used, with 
 import gym
+
+based on "Getting Started With OpenAI Gym"
+https://www.youtube.com/watch?v=8MC3y7ASoPs
+
+And the playlist by the same author:
+https://www.youtube.com/playlist?list=PLIfPjWrv526bMF8_vx9BqWjec-F-g-lQO
 '''
+debug_mode = [2] # initializing an empty array for debugging purposes
 
 # debug block 1 ~~~~~~~~~~~~~~~~~~~~~~~~
 
+import gym
 import random
+import numpy as np
 
 # debug block 2 ~~~~~~~~~~~~~~~~~~~~~~~~
 
 class Agent():
-  def __init__(self, enviro): # inputs: environment
-    self.action_size = enviro.action_space.n
-    print("Action Size:\t", self.action_size)
-    
-  def get_action(self, state, strat = 'random'): 
-  # inputs: state of the environment; a list
+  def __init__(self, enviro, strat): 
+  # inputs: environment
   #         strategy to use for picking an action.  defaults to "random".
-    if strat == 'simple':
-      if state[2] < 0: # if pole angle is tilting left
-        action = 0
+    self.is_discrete = type(enviro.action_space) == gym.spaces.discrete.Discrete
+    if self.is_discrete:
+      self.action_size = enviro.action_space.n
+    else:
+      self.action_low = enviro.action_space.low
+      self.action_high = enviro.action_space.high
+      self.action_shape = enviro.action_space.shape
+      
+    self.behav = strat
+
+    if 2 in debug_mode:
+      if self.is_discrete:
+        print("Action Size:\t", self.action_size)
       else:
-        action = 1
+        print("Action range:\t", self.action_low, self.action_high)
+      print(strat)
+    
+  def get_action(self, state): 
+  # inputs: state of the environment; a list  
+    if self.behav == 'simple':
+      if self.is_discrete:  
+        if state[2] < 0: # if pole angle is tilting left, for example
+          #CHANGE_ME: not all games have the same state space!!!
+          action = 0 # this corresponds to a left motion in the action table
+        else:
+          action = 1
+      else:
+        action = [0] # CHANGE THIS
+
     else: # strat defaults to random
-      action = random.choice(range(self.action_size))
+      if self.is_discrete:  
+        action = random.choice(range(self.action_size))
+      else:
+        action = np.random.uniform(self.action_low, \
+                                   self.action_high, \
+                                   self.action_shape)
     return action
--- openAiGymDemo.py
+++ openAiGymDemo.py
@@ -4,15 +4,18 @@
 openAiGymDemo.py
 Author: Miles Kovach
 Created: 2020-10-09
-Modified: 2020-10-17
+Modified: 2020-10-18
 EC601 Assignment big, script 0.1
 
 A program to test openAIgym
 based on "Getting Started With OpenAI Gym"
 https://www.youtube.com/watch?v=8MC3y7ASoPs
+
+And the playlist by the same author:
+https://www.youtube.com/playlist?list=PLIfPjWrv526bMF8_vx9BqWjec-F-g-lQO
 '''
 
-debug_mode = [1] # initializing an empty array for debugging purposes
+debug_mode = [1, 2.1] # initializing an empty array for debugging purposes
 
 # debug block 0 ~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -21,32 +24,45 @@ import agentTypes as aty
 
 # debug block 1 ~~~~~~~~~~~~~~~~~~~~~~~~
 
-env_name = 'CartPole-v1'
+env_name = 'MountainCarContinuous-v0' # what game is being played
+# including but not limited to:
+# 'CartPole-v1', 'MountainCar-v0', 'MountainCarContinuous-v0'
 env = gym.make(env_name)
-actsLike = 'simple'
+actsLike = 'simple' # what kind of agent
+# including but not limited to:
+# 'random', 'simple'
 
-currAgnt = aty.Agent(env) # initializing an agent
+if 1 in debug_mode:
+  print("Observation space:\t", env.observation_space)
+  print("Action space:\t", env.action_space)
+
+currAgnt = aty.Agent(env, actsLike) # initializing an agent
+
+# debug block 2 ~~~~~~~~~~~~~~~~~~~~~~~~
 
 thisState = env.reset()
-thisAction = currAgnt.get_action(thisState, actsLike)
+thisAction = currAgnt.get_action(thisState)
+SRDI = env.step(thisAction)
+# SRDI is a list containing four parameters [S, R, D, I]:
+# (S)tate, a description of what the agent knows about its environment
+# (R)eward, the reward the agent gets for taking the last step it took
+# (D)one, a bool which is set when any of the game's termination conditions
+# ...are met
+# (I)nformation, any additional info the agent provides.
 
-for q in range(200):
+actionsTaken = 1
+
+while not(SRDI[2]):
   SRDI = env.step(thisAction)
-  # SRDI is a list containing four parameters [S, R, D, I]:
-  # (S)tate, a description of what the agent knows about its environment
-  # (R)eward, the reward the agent gets for taking the last step it took
-  # (D)one, a bool which is set when any of the game's termination conditions
-  # ...are met
-  # (I)nformation, any additional info the agent provides.
+  actionsTaken += 1
 
-  if SRDI[2] == 1: # breaks the loop if done flag is set
-    print('done')
-    break
-
-  thisAction = currAgnt.get_action(SRDI[0], actsLike)
+  thisAction = currAgnt.get_action(SRDI[0])
   env.render()
   
-  if 1 in debug_mode:
-    print(q, SRDI)
-    
+  if 2.0 in debug_mode:
+    print(SRDI)
+
+if 2.1 in debug_mode:
+  print("actionsTaken:\t", actionsTaken)
+
 env.close()
